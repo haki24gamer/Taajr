d209564794c89b89d98eb3562bfedde0c5bdd94e
Habib566513 <habibalimohamed92@gmail.com>
2024-11-27 08:52:22 +0100

    modified:   app.py 	new file:   templates/Vendeur.html

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/Panier')
+def Panier():
+    return render_template("Panier.html")
+
+@app.route('/Vendeur')
+def Vendeur():
+    return render_template("Vendeur.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
